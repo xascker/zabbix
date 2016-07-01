3628ad0106a3a8a0efb0b6da2fdb8ca336f20176
Zabbix Backup <backup@mail.com>
2016-07-01 10:17:51 +0000

    Export templates from Июл 01, 2016

--- zbx_import.sh
+++ zbx_import.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-
-git rev-parse HEAD > newversion && diff newversion version
-git rev-parse HEAD > version
-
-
-git fetch
-if $(git rev-parse HEAD) == $(git rev-parse @{u});
-then
-    echo "not need pull"
-else
-    echo "pull"
-   
-fi
-
 #git pull origin master
+
+#git rev-parse HEAD > newversion && diff newversion version
+#git rev-parse HEAD > version
+
+
 #perl import.pl
+
+
+
+result=$(diff newversion version)
+
+if [ $? -eq 0]; then
+echo "files are the same"
+else
+echo "files are different"
+echo "$result"
+fi
