f7d43f004be393570696236330b4dad688fffee3
Zabbix Backup <backup@mail.com>
2016-07-01 10:40:19 +0000

    Export templates from Июл 01, 2016

--- zbx_import.sh
+++ zbx_import.sh
@@ -7,9 +7,9 @@ if result="$(diff newversion version)"
   then
     echo "Import nothing"
   else
-    git rev-parse HEAD > version
     echo "Are imported:"
     perl import.pl
+    git rev-parse HEAD > version
 fi
 
 
